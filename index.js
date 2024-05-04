c4a5eafe9bcb5974ff69bebca608454c9a3fd7db
mstfandiaye <mstfa.ndiaye23@gmail.com>
2024-05-04 13:02:58 +0000

    update html and json content

--- index.js
+++ index.js
@@ -1,48 +1,30 @@
-// import express
-const express = require('express') ;
-//create path
-const path =require('path') ;
-// define app
-const app = express() ;
-// define the port for listening
-const port = 3000 ;
-//path usage
-app.use(express.static(path.join(__dirname, 'static'))) ;
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/static', (req, res) => {
-    res.sendFile(path.join(__dirname, 'static', 'login.html')) ;
+app.get('/', (req, res) => {
+    const HTMLcontent = `
+    <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+<body>
+    <div>here is an html content</div>
+</body>
+</html>
+    `
+    //res.send(HTMLcontent);
+    const JsonContent = `
+        {
+            'key', 'value'
+        }
+    `
+    res.send(JsonContent) ;
 });
 
-//middle were
-//getting requeste
-app.get('/', (req, res)=> {
-    // for getting res in text format
-    const contentText = 'hello, here is my first outpout in express' ;
-    //res.send(contentText) ;
-    // html format
-    const contentHTML = `
-    <html>
-        <head>
-            <h1>My html content</h1>
-        </head>
-        <body>
-            <div>hello, here is a HTML content</div>
-        </body>
-    </html>
-    `
-    //res.send(contentHTML) ;
-    // Json content 
-    const conentJson = `
-    {
-        'message' : 'here is a Json content'
-    }
-    `
-    res.set('content-type', 'applicatiion/Json') ;
-    res.statusCode = 201;
-    //res.send(conentJson) ;
-})
-
-//listening requeste
 app.listen(port, () => {
-    console.log(`app is on port ${port}`) 
-})
+    console.log(`on port ${port}`);
+});
